172031b8437d61e5ec87cbcd3275fd6cf608d046
François Devémy <francois.devemy@reach5.co>
2024-11-20 18:44:59 +0100

    Annuler les modifications de la sandbox pour l'instant

--- Sandbox/Sandbox/AppDelegate.swift
+++ Sandbox/Sandbox/AppDelegate.swift
@@ -57,7 +57,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     )
 
     #if targetEnvironment(macCatalyst)
-    static let macProviders: [ProviderCreator] = [GoogleProvider(variant: "one_tap"), FacebookProvider(prefersLoginTracking: .enabled), AppleProvider(variant: "natif")]
+    static let macProviders: [ProviderCreator] = [GoogleProvider(variant: "one_tap"), FacebookProvider(variant: "variant_1"), AppleProvider(variant: "natif")]
     static let macLocal: ReachFive = ReachFive(sdkConfig: sdkLocal, providersCreators: macProviders, storage: storage)
     static let macRemote: ReachFive = ReachFive(sdkConfig: sdkRemote, providersCreators: macProviders, storage: storage)
     let reachfive = macLocal
