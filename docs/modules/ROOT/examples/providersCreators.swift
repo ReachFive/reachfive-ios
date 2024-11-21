fc6bbd0729e18702ab21057a4573797421813b81
François Devémy <francois.devemy@reach5.co>
2024-11-21 18:04:17 +0100

    Toto

--- docs/modules/ROOT/examples/providersCreators.swift
+++
@@ -1,8 +0,0 @@
-static let reachfive: ReachFive = ReachFive(
-    sdkConfig: sdkRemote,
-    providersCreators: [
-        GoogleProvider(), // first variant containing ios in its name
-        FacebookProvider(variant: "ios_android"),
-        AppleProvider(variant: "ios_uat"), // nouvelle classe AppleProvider à créer
-        WeChat(variant: "ios17")]
-)
