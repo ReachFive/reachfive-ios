ec9783eec66640bd28370fe96686b95b9b69801d
François Devémy <francois.devemy@reach5.co>
2025-07-07 22:11:29 +0200

    moins de Result

--- Sources/Core/Classes/utils/DataRequest.swift
+++ Sources/Core/Classes/utils/DataRequest.swift
@@ -3,23 +3,22 @@ import Alamofire
 
 
 extension DataRequest {
-
+    
     private func isSuccess(_ status: Int?) -> Bool {
         guard let status else {
             return false
         }
         return status >= 200 && status < 300
     }
-
-    private func parseJson<T: Decodable>(json: Data, type: T.Type, decoder: JSONDecoder) -> Result<T, ReachFiveError> {
+    
+    private func parseJson<T: Decodable>(json: Data, type: T.Type, decoder: JSONDecoder) throws -> T {
         do {
-            let value = try decoder.decode(type, from: json)
-            return .success(value)
+            return try decoder.decode(type, from: json)
         } catch {
-            return .failure(.TechnicalError(reason: error.localizedDescription))
+            throw ReachFiveError.TechnicalError(reason: error.localizedDescription)
         }
     }
-
+    
     private func handleResponseStatus(status: Int?, apiError: ApiError) -> ReachFiveError {
         guard let status else {
             return .TechnicalError(
@@ -37,48 +36,53 @@ extension DataRequest {
             apiError: apiError
         )
     }
-
+    
     func responseJson(decoder: JSONDecoder) async throws -> Void {
         return try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<Void, Error>) in
             responseData { responseData in
                 switch responseData.result {
                 case let .failure(error):
                     continuation.resume(throwing: ReachFiveError.TechnicalError(reason: error.localizedDescription))
-
+                    
                 case let .success(data):
                     let status = responseData.response?.statusCode
                     if self.isSuccess(status) {
                         continuation.resume(returning: ())
                     } else {
-                        switch self.parseJson(json: data, type: ApiError.self, decoder: decoder) {
-                        case .success(let value): continuation.resume(throwing: self.handleResponseStatus(status: status, apiError: value))
-                        case .failure(let error): continuation.resume(throwing: error)
+                        do {
+                            let apiError = try self.parseJson(json: data, type: ApiError.self, decoder: decoder)
+                            continuation.resume(throwing: self.handleResponseStatus(status: status, apiError: apiError))
+                        } catch {
+                            continuation.resume(throwing: error)
                         }
                     }
                 }
             }
         }
     }
-
+    
     func responseJson<T: Decodable>(type: T.Type, decoder: JSONDecoder) async throws -> T {
         return try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<T, Error>) in
             responseData { responseData in
                 switch responseData.result {
                 case let .failure(error):
                     continuation.resume(throwing: ReachFiveError.TechnicalError(reason: error.localizedDescription))
-
+                    
                 case let .success(data):
                     let status = responseData.response?.statusCode
-                    if self.isSuccess(status) {
-                        continuation.resume(with: self.parseJson(json: data, type: T.self, decoder: decoder))
-                    } else {
-                        switch self.parseJson(json: data, type: ApiError.self, decoder: decoder) {
-                        case .success(let value): continuation.resume(throwing: self.handleResponseStatus(status: status, apiError: value))
-                        case .failure(let error): continuation.resume(throwing: error)
+                    do {
+                        if self.isSuccess(status) {
+                            continuation.resume(returning: try self.parseJson(json: data, type: T.self, decoder: decoder))
+                        } else {
+                            let apiError = try self.parseJson(json: data, type: ApiError.self, decoder: decoder)
+                            continuation.resume(throwing: self.handleResponseStatus(status: status, apiError: apiError))
                         }
+                    } catch {
+                        continuation.resume(throwing: error)
                     }
                 }
             }
         }
     }
 }
+    
