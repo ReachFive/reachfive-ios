18c0dc7bb10deb3916a97c36fd6660129d7e1e43
François Devémy <francois.devemy@reach5.co>
2025-07-11 12:10:07 +0200

    PasskeyAutoFillController

--- Sandbox/Sandbox/PasskeyAutoFillController.swift
+++ Sandbox/Sandbox/PasskeyAutoFillController.swift
@@ -1,18 +1,18 @@
 import Foundation
 import Reach5
 
-class PasskeyAutoFillControler: UIViewController {
+class PasskeyAutoFillController: UIViewController {
 
     #if !targetEnvironment(macCatalyst)
         override func viewDidAppear(_ animated: Bool) {
             super.viewDidAppear(animated)
-            print("PasskeyAutoFillControler.viewDidAppear")
+            print("PasskeyAutoFillController.viewDidAppear")
 
             if #available(iOS 16.0, *) {
                 Task { @MainActor in
                     guard let window = view.window else { fatalError("The view was not in the app's view hierarchy!") }
                     await handleAuthToken {
-                        try await AppDelegate.reachfive().beginAutoFillAssistedPasskeyLogin(withRequest: NativeLoginRequest(anchor: window, origin: "PasskeyAutoFillControler.viewDidAppear"))
+                        try await AppDelegate.reachfive().beginAutoFillAssistedPasskeyLogin(withRequest: NativeLoginRequest(anchor: window, origin: "PasskeyAutoFillController.viewDidAppear"))
                     }
                 }
             }
