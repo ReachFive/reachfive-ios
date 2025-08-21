bbdbb22f131434fac15c127e5d2c0c32640d5861
François Devémy <francois.devemy@reach5.co>
2025-08-21 17:51:45 +0200

    De manière défensive, j'appèle le callback de session dans le thread principal.

--- Sources/Core/Classes/utils/WebAuthentication.swift
+++ Sources/Core/Classes/utils/WebAuthentication.swift
@@ -9,27 +9,30 @@ func webAuthenticationSession(url: URL, callbackURLScheme: String, presentationC
         
         // Initialize the session.
         let session = ASWebAuthenticationSession(url: url, callbackURLScheme: callbackURLScheme) { callbackURL, error in
-            guard !hasResumed else {
-                // Already resumed, so we can ignore this call.
-                return
-            }
-            hasResumed = true
-            
-            if let error {
-                let r5Error: ReachFiveError = switch error._code {
-                case 1: .AuthCanceled
-                case 2: .TechnicalError(reason: "Presentation Context Not Provided")
-                case 3: .TechnicalError(reason: "Presentation Context Invalid")
-                default:.TechnicalError(reason: "Unknown Error \(error.localizedDescription)")
+            // Ensure that all logic is executed on the main thread to prevent race conditions.
+            Task { @MainActor in
+                guard !hasResumed else {
+                    // Already resumed, so we can ignore this call.
+                    return
                 }
-                continuation.resume(throwing: r5Error)
-                return
+                hasResumed = true
+                
+                if let error {
+                    let r5Error: ReachFiveError = switch error._code {
+                    case 1: .AuthCanceled
+                    case 2: .TechnicalError(reason: "Presentation Context Not Provided")
+                    case 3: .TechnicalError(reason: "Presentation Context Invalid")
+                    default:.TechnicalError(reason: "Unknown Error \(error.localizedDescription)")
+                    }
+                    continuation.resume(throwing: r5Error)
+                    return
+                }
+                guard let callbackURL else {
+                    continuation.resume(throwing: ReachFiveError.TechnicalError(reason: "No callback URL"))
+                    return
+                }
+                continuation.resume(returning: callbackURL)
             }
-            guard let callbackURL else {
-                continuation.resume(throwing: ReachFiveError.TechnicalError(reason: "No callback URL"))
-                return
-            }
-            continuation.resume(returning: callbackURL)
         }
 
         // Set an appropriate context provider instance that determines the window that acts as a presentation anchor for the session
